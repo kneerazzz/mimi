4d22d4536f1bb03d428eae1c9eb40d705e54899c
kneerazzz <troyyig@gmail.com>
2025-11-10 16:03:27 +0530

    made the user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,27 @@
 import app from "./app.js";
 import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './.env'
 })
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("<h1>mimi server is running wohohohho</h1>")
-})
+connectDB()
+.then(() => {
+    app.get("/", (req, res) => {
+        res.send("<h1>mimi server is running wohohohho</h1>")
+    })
 
-app.get("/meme", (req, res) => {
-    res.send("<h2>yo its actually working</h2>")
-})
+    app.get("/meme", (req, res) => {
+        res.send("<h2>yo its actually working</h2>")
+    })
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-    console.log(`http://localhost:${PORT}`);
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`)
+        console.log(`http://localhost:${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("connection database failed!", error)
 })
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { v4 as uuidv4 } from 'uuid'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+    },
+    uuid: {
+        type: String,
+        required: true,
+        default: uuidv4
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true,
+        sparse: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+        sparse: true
+    },
+    password: {
+        type: String,
+    },
+    bio: {
+        type: String,
+        default: "holy moly"
+    },
+    profilePic: {
+        type: String,
+        default: "https://res.cloudinary.com/dmrf8lhcf/image/upload/v1762762960/Luffy__3_uppff0.jpg"
+    },
+    is_registered: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified("password") || !this.password ){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
