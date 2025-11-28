816facca7a940f651cac69eecbecc2d701ebb89d
kneerazzz <troyyig@gmail.com>
2025-11-28 20:52:53 +0530

    made the register controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
-import {apiError} from '../utils/apiError.js'
-import { apiResponse } from '../utils/apiResponse.js'
-import { ModifiedPathsSnapshot } from 'mongoose';
+import { ApiError} from '../utils/apiError.js'
+import { ApiResponse } from '../utils/apiResponse.js'
 import { User } from '../models/user.model.js';
 
 const generateAccessAndRefreshTokens = async(userId) => {
@@ -13,6 +12,10 @@ const generateAccessAndRefreshTokens = async(userId) => {
         
         await user.save({validateBeforeSave: false})
         return {accessToken, refreshToken}
+    }
+    catch(error){
+        console.log("Error generating tokens")
+        throw new ApiError(500, "Something went wrong while generating tokens!!")
     }
 }
 
@@ -26,7 +29,7 @@ const registerUser = asyncHandler(async(req, res) => {
     if(!name || !username || !email || !password ||
         [name, username, email, password].some((index) => index?.trim() === "")
     ){
-        throw new apiError(400, "All registeration fields are required!")
+        throw new ApiError(400, "All registeration fields are required!")
     }
 
     const existedUser = await User.findOne({
@@ -38,12 +41,43 @@ const registerUser = asyncHandler(async(req, res) => {
     })
     
     if(existedUser){
-        throw new apiError(409, "User with similar username or email already exists")
+        throw new ApiError(409, "User with similar username or email already exists")
     }
     anonuser.name = name;
     anonuser.email = email;
     anonuser.username = username;
     anonuser.password = password;
+    anonuser.is_registered = true;
 
-    const newRegisteredUser = await anonuser.save();
+    await anonuser.save();
+
+    const registeredUser = await User.findById(anonuser._id).select("-password -refreshToken")
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(anonuser._id);
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    }
+    if(!registeredUser){
+        throw new ApiError(500, "Something went wrong while creating user")
+    }
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, {
+        ...options,
+        maxAge: 24 * 60 * 60 * 1000
+    })
+    .cookie("refreshToken", refreshToken, {
+        ...options,
+        maxAge: 15 * 24 * 60 * 60 * 1000
+    })
+    .json(
+        new ApiResponse(200, {user: registeredUser}, "User registered successfully")
+    )
+
 })
+
+
+
+export {registerUser}
